379283bc249ec22989a750707f2a05e7182d3484
Zowie <git@zowie.cx>
2024-12-12 19:45:29 +0100

    fix(clippy): remove unneeded return statement in `hex_to_color`

--- src/util.rs
+++ src/util.rs
@@ -2,13 +2,13 @@ use pkrs::model::Member;
 use poise::serenity_prelude::{self as serenity};
 
 pub(crate) fn hex_to_color(hex: Option<String>) -> serenity::Colour {
-    return match hex {
+    match hex {
         Some(hex) => serenity::Colour(
             u32::from_str_radix(hex.trim_start_matches("#"), 16)
                 .unwrap_or(serenity::colours::roles::DEFAULT.0),
         ),
         None => serenity::colours::roles::DEFAULT,
-    };
+    }
 }
 
 pub(crate) fn get_member_name(member: &Member) -> String {
